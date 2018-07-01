d413580d3253fd4f56967f6d1f7e551028313b7f
Martin <martinvalchev2@gmail.com>
2018-07-01 22:28:28 +0300

    added JS Classes - Lab

---
+++ JS Advanced/JS Classes - Lab/Get Persons.js
@@ -0,0 +1,23 @@
+function createPerson() {
+    class Person {
+        constructor(fName, lName, age, email) {
+            this.firstName = fName
+            this.lastName = lName
+            this.age = age
+            this.email = email
+        }
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    return [
+        new Person ('Maria', 'Petrova', 22, 'mp@yahoo.com'),
+        new Person('SoftUni'),
+        new Person('Stephan', 'Nikolov', 25),
+        new Person('Peter', 'Kolev', 24, 'ptr@gmail.com'),
+    ]
+}
+
+createPerson().forEach(p => console.log(p.toString()))
+
