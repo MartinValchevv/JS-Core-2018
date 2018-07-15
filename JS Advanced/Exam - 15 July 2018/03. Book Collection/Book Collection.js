c498b0aee0fdb49b39cf9c11c522b9c4ec3e1e8a
Martin <martinvalchev2@gmail.com>
2018-07-16 00:25:45 +0300

    added Exam - 15 July 2018/03. Book Collection and 04. Online Shop

--- JS Advanced/Exam - 15 July 2018/03. Book Collection/Book Collection.js
+++ JS Advanced/Exam - 15 July 2018/03. Book Collection/Book Collection.js
@@ -1,8 +1,78 @@
 class BookCollection {
-    constructor (room, shelfGenre, shelf, shelfCapacity) {
+    constructor (shelfGenre, room, shelfCapacity) {
         this.room = room
         this.shelfGenre = shelfGenre
-        this.shelf = shelf
-        this.shelfCapacity = shelfCapacity
+        this.shelfCapacity = +shelfCapacity
+        this.shelf = []
+    }
+
+    get room() {
+        return this._room
+    }
+
+    get shelfCondition () {
+        this._shelfCondition = this.shelfCapacity - this.shelf.length
+        return this._shelfCondition
+    }
+
+    set room(value) {
+        if (value === 'livingRoom' || value === 'bedRoom' || value === 'closet') {
+            this._room = value
+            return
+        }
+        throw new Error(`Cannot have book shelf in ${value}`)
+    }
+
+    addBook(bookName, bookAuthor, genre) {
+        let object = {bookName: bookName, bookAuthor: bookAuthor, genre: genre}
+        this.shelf.push(object)
+        return this
+    }
+
+    throwAwayBook(bookName) {
+        this.shelf = this.shelf.filter(a=>a.bookName !== bookName)
+    }
+
+    showBooks(genre) {
+        let arr = []
+        arr = this.shelf.filter(a=>a.genre === genre)
+        let output = `Results for search ${genre}":\n`
+        for (let obj of arr) {
+            output += `\n “\uD83D\uDCD6 "${obj.bookName}" – ${obj.bookAuthor}”`
+        }
+        output = output.slice(0, -1)
+        return output
+    }
+
+    toString() {
+        let result = `"${this.shelfGenre}" shelf in ${this.room} contains:`
+        if (this.shelf.length > 0) {
+            this.shelf.forEach(o => result += `\n “\uD83D\uDCD6 "${o.bookName}" – ${o.bookAuthor}”`);
+        } else {
+            // result += "\n";
+            return 'It\'s an empty shelf'
+        }
+        return result;
     }
 }
+
+
+// let livingRoom = new BookCollection("Programming", "livingRoom", 5)
+//     .addBook("Introduction to Programming with C#", "Svetlin Nakov")
+//     .addBook("Introduction to Programming with Java", "Svetlin Nakov")
+//     .addBook("Programming for .NET Framework", "Svetlin Nakov");
+// console.log(livingRoom.toString());
+
+
+// let garden = new BookCollection("Programming", "garden");
+// console.log(garden.toString());
+
+let bedRoom = new BookCollection('Mixed', 'bedRoom', 5);
+bedRoom.addBook("John Adams", "David McCullough", "history");
+bedRoom.addBook("The Guns of August", "Cuentos para pensar", "history");
+bedRoom.addBook("Atlas of Remote Islands", "Judith Schalansky");
+bedRoom.addBook("Paddle-to-the-Sea", "Holling Clancy Holling");
+console.log("Shelf's capacity: " + bedRoom.shelfCondition);
+console.log(bedRoom.showBooks("history"));
+
+
---
+++ JS Advanced/Exam - 15 July 2018/04. Online Shop/solution.js
@@ -0,0 +1,53 @@
+function onlineShop(selector) {
+    let form = `<div id="header">Online Shop Inventory</div>
+    <div class="block">
+        <label class="field">Product details:</label>
+        <br>
+        <input placeholder="Enter product" class="custom-select">
+        <input class="input1" id="price" type="number" min="1" max="999999" value="1"><label class="text">BGN</label>
+        <input class="input1" id="quantity" type="number" min="1" value="1"><label class="text">Qty.</label>
+        <button id="submit" class="button" disabled>Submit</button>
+        <br><br>
+        <label class="field">Inventory:</label>
+        <br>
+        <ul class="display">
+        </ul>
+        <br>
+        <label class="field">Capacity:</label><input id="capacity" readonly>
+        <label class="field">(maximum capacity is 150 items.)</label>
+        <br>
+        <label class="field">Price:</label><input id="sum" readonly>
+        <label class="field">BGN</label>
+    </div>`;
+    $(selector).html(form);
+    let product = $('input.custom-select')
+    let price = $('#price')
+    let quantity = $('#quantity')
+    let submit = $('#submit')
+    let capacity = $('#capacity')
+    let sum = $('#sum')
+    let priceSum = 0
+    let capacitySum = 0
+
+    product.on('input', function () {
+        submit.removeAttr('disabled')
+    })
+    submit.on('click', function () {
+        capacitySum += Number(quantity.val())
+        priceSum += Number(price.val())
+            $('ul.display').append($('<li>').text(`Product: ${product.val()} Price: ${price.val()} Quantity: ${quantity.val()}`))
+            if (capacitySum >= 150) {
+            capacity.val('full').addClass('fullCapacity')
+                product.attr('disabled', '')
+                price.attr('disabled', '')
+                quantity.attr('disabled', '')
+                submit.off()
+            } else {
+            capacity.val(capacitySum)
+                sum.val(priceSum)
+            }
+            product.val(undefined)
+        price.val(1)
+        quantity.val(1)
+    })
+}
