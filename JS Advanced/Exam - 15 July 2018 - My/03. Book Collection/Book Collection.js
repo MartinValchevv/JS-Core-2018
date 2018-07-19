4fc4ce78138b840151f2319ed232fc7bebcb1f1b
Martin <martinvalchev2@gmail.com>
2018-07-19 15:22:59 +0300

    fix 03. Book Collection.js

--- JS Advanced/Exam - 15 July 2018 - My/03. Book Collection/Book Collection.js
+++ JS Advanced/Exam - 15 July 2018 - My/03. Book Collection/Book Collection.js
@@ -1,59 +1,87 @@
 class BookCollection {
-    constructor (shelfGenre, room, shelfCapacity) {
-        this.room = room
-        this.shelfGenre = shelfGenre
-        this.shelfCapacity = +shelfCapacity
-        this.shelf = []
+
+    constructor(shelfGenre, room, shelfCapacity) {
+        this.room = room;
+        this.shelfGenre = shelfGenre;
+        this.shelf = [];
+        this.shelfCapacity = shelfCapacity;
+        this._shelfCondition = 0;
     }
 
     get room() {
-        return this._room
+        return this._room;
     }
 
-    get shelfCondition () {
-        this._shelfCondition = this.shelfCapacity - this.shelf.length
-        return this._shelfCondition
+    get shelfCondition() {
+        this._shelfCondition = this.shelfCapacity - this.shelf.length;
+        return this._shelfCondition;
     }
 
     set room(value) {
         if (value === 'livingRoom' || value === 'bedRoom' || value === 'closet') {
-            this._room = value
-            return
+            this._room = value;
+            return;
         }
         throw new Error(`Cannot have book shelf in ${value}`)
     }
 
-    addBook(bookName, bookAuthor, genre) {
-        let object = {bookName: bookName, bookAuthor: bookAuthor, genre: genre}
-        this.shelf.push(object)
-        return this
+    addBook() {
+        let obj = {};
+        let bookName, bookAuthor, genre;
+        if (arguments.length === 3) {
+            bookName = arguments[0];
+            bookAuthor = arguments[1];
+            genre = arguments[2];
+            obj = {bookName, bookAuthor, genre}
+        } else if (arguments.length === 2) {
+            bookName = arguments[0];
+            bookAuthor = arguments[1];
+            obj = {bookName, bookAuthor}
+        }
+
+        if (this.shelfCondition === 0) {
+            this.shelf.shift();
+            this.shelf.push(obj)
+        } else {
+            this.shelf.push(obj)
+        }
+
+        this.shelf = this.shelf.sort((a, b) => a.bookAuthor.localeCompare(b.bookAuthor));
+
+        return this;
     }
 
     throwAwayBook(bookName) {
-        this.shelf = this.shelf.filter(a=>a.bookName !== bookName)
+        this.shelf = this.shelf.filter(a => a.bookName !== bookName)
     }
 
     showBooks(genre) {
-        let arr = []
-        arr = this.shelf.filter(a=>a.genre === genre)
-        let output = `Results for search ${genre}":\n`
+        let arr = [];
+        arr = this.shelf.filter(a => a.genre === genre);
+        let output = `Results for search "${genre}":\n`;
+
         for (let obj of arr) {
-            output += `\n “\uD83D\uDCD6 "${obj.bookName}" – ${obj.bookAuthor}”`
+            output += `\uD83D\uDCD6 ${obj.bookAuthor} - "${obj.bookName}"\n`
         }
-        output = output.slice(0, -1)
-        return output
+        output = output.slice(0, -1);
+        return output;
     }
 
     toString() {
-        let result = `"${this.shelfGenre}" shelf in ${this.room} contains:`
-        if (this.shelf.length > 0) {
-            this.shelf.forEach(o => result += `\n “\uD83D\uDCD6 "${o.bookName}" – ${o.bookAuthor}”`);
-        } else {
-            // result += "\n";
-            return 'It\'s an empty shelf'
+        let output = `"${this.shelfGenre}" shelf in ${this._room} contains:\n`;
+
+        if (this.shelf.length === 0) {
+            return `It's an empty shelf`
         }
-        return result;
+
+
+        for (let obj of this.shelf) {
+            output += `\uD83D\uDCD6 "${obj.bookName}" - ${obj.bookAuthor}\n`
+        }
+        output = output.slice(0, -1);
+        return output
     }
+
 }
 
 
